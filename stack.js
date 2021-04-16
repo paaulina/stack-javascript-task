9edd04aac81d9d111434e6da512088677e3ebf99
Paulina Jędrzejewska <p.jedrzejewska97@gmail.com>
2021-04-16 10:15:16 +0200

    Add stack implementation

--- stack.js
+++ stack.js
@@ -1,1 +1,53 @@
-// Your implementation goes here
+class Stack {
+    constructor() {
+        this.content = [];
+        for (const a in arguments) {
+            this
+                .content
+                .push(arguments[a]);
+        }
+
+    }
+
+    get size() {
+        return this.content.length;
+    }
+
+    push = (element) => {
+        this
+            .content
+            .push(element);
+
+    }
+
+    pop = () => {
+        if (this.isEmpty()) 
+            return null;
+        return this
+            .content
+            .pop();
+    }
+
+    peek = () => {
+        if (this.isEmpty()) 
+            return null;
+        return this.content[this.size - 1];
+    }
+
+    swap = () => {
+        if (this.size < 2) {
+            return;
+        }
+
+        let last = this.content[this.size - 1];
+        this.content[this.size - 1] = this.content[this.size - 2];
+        this.content[this.size - 2] = last;
+    }
+
+    isEmpty = () => {
+        return this.size === 0;
+    }
+
+}
+
+module.exports = Stack;
